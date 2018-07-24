f31b215e53760f5d127b12e72854bab8fa628e6c
lifan <554572350@qq.com>
2018-07-24 17:25:41 +0800

    Concentration

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by lifan on 2018/7/24.
+//  Copyright © 2018 lifan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    var flipCount = 0 {
+        didSet {
+            flipCountLable.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLable: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+
+
+}
+
